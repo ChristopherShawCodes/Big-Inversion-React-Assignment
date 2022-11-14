fafa9d36b33797ba2bb25244e5b2b25cdbd65f08
Christopher Shaw <silvertn350@yahoo.com>
2022-11-14 09:49:18 -0600

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import PersonCard from './Components/PersonCard'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <PersonCard firstName='Christopher' lastName='Shaw' age = {33} hairColor = 'brown'/>
+    <PersonCard firstName='Brittany' lastName='Shaw' age = {32} hairColor = 'black'/>
+    <PersonCard firstName='Alyssa' lastName='Shaw' age = {12} hairColor = 'black'/>
+    <PersonCard firstName='Aiden' lastName='Shaw' age = {9} hairColor = 'black'/>
+
     </div>
   );
 }
